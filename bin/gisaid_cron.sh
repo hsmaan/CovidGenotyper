ae4968b0794983b859a1b901be8c13e3f76687e2
hsmaan <hmaan@uoguelph.ca>
2020-05-08 18:10:40 -0400

    migrated processing to GCE

--- bin/gisaid_cron.sh
+++ bin/gisaid_cron.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-module load R/3.6.1
+cd /home/hmaan/covid_shiny/bin
 
-sbatch --mem=16G -J cv_meta -t 5-00:00:00 -c 12 Rscript --verbose metadata_process.R
+# Run metadata and sequence processing scripts
 
-sleep 1h 
+Rscript --verbose metadata_process.R
 
-sbatch -p himem --mem=50G -J cv_msa -t 5-00:00:00 -c 12 Rscript --verbose gisaid_sequence_process.R
+Rscript --verbose gisaid_sequence_process.R
+
+# Run snp processing scripts
+
+sh snp_sites_process.sh
+
+Rscript --verbose maf_sites_out.R
+
+# Preprocess the plot data
+
+Rscript --verbose preprocess_plot_data.R
+
+# Delete old docker image and rebuild new. Push new image to google container reg
+
+docker image rm gcr.io/bo-wang-274419/cgt 
+
+docker system prune --force
+
+cd ..
+
+docker build -t gcr.io/bo-wang-274419/cgt -f app.Dockerfile .
+
+docker push gcr.io/bo-wang-274419/cgt
 
