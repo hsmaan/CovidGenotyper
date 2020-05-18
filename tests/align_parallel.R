cfd43b9d3301f8949a953b9dcebab44a917a4d45
hsmaan <hmaan@uoguelph.ca>
2020-05-18 16:55:53 +0000

    spawn parallel proc sep align_test

--- tests/align_parallel.R
+++ tests/align_parallel.R
@@ -48,13 +48,13 @@ print(align_comp)
 
 tt2_1 <- Sys.time()
 
-ua_subsets <- split(unaligned, ceiling(seq_along(unaligned)/50))
+ua_subsets_2 <- split(unaligned, ceiling(seq_along(unaligned)/50))
 
-nogap_subsets <- mclapply(ua_subsets, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
+nogap_subsets_2 <- mclapply(ua_subsets_2, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
 
-align_subsets <- mclapply(nogap_subsets, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
+align_subsets_2 <- mclapply(nogap_subsets_2, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
 
-align_comp <- base::Reduce(align_profs, align_subsets)
+align_comp_2 <- base::Reduce(align_profs, align_subsets_2)
 
 tt2_2 <- Sys.time()
 
@@ -62,19 +62,19 @@ tt2_diff <- tt2_2 - tt2_1
 
 print(tt2_diff)
 
-print(align_comp)
+print(align_comp_2)
 
 # Test 3 - Size 25 chunks 
 
 tt3_1 <- Sys.time()
 
-ua_subsets <- split(unaligned, ceiling(seq_along(unaligned)/25))
+ua_subsets_3 <- split(unaligned, ceiling(seq_along(unaligned)/25))
 
-nogap_subsets <- mclapply(ua_subsets, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
+nogap_subsets_3 <- mclapply(ua_subsets_3, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
 
-align_subsets <- mclapply(nogap_subsets, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
+align_subsets_3 <- mclapply(nogap_subsets_3, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
 
-align_comp <- base::Reduce(align_profs, align_subsets)
+align_comp_3 <- base::Reduce(align_profs, align_subsets_3)
 
 tt3_2 <- Sys.time()
 
@@ -82,19 +82,19 @@ tt3_diff <- tt3_2 - tt3_1
 
 print(tt3_diff)
 
-print(align_comp)
+print(align_comp_3)
 
 # Test 4 - Size 10 chunks 
 
 tt4_1 <- Sys.time()
 
-ua_subsets <- split(unaligned, ceiling(seq_along(unaligned)/10))
+ua_subsets_4 <- split(unaligned, ceiling(seq_along(unaligned)/10))
 
-nogap_subsets <- mclapply(ua_subsets, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
+nogap_subsets_4 <- mclapply(ua_subsets_4, function(x) RemoveGaps(x, removeGaps = "all", processors = 2), mc.cores = cores)
 
-align_subsets <- mclapply(nogap_subsets, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
+align_subsets_4 <- mclapply(nogap_subsets_4, function(x) AlignSeqs(x, iterations = 0, refinements = 0, processors = 2), mc.cores = cores)
 
-align_comp <- base::Reduce(align_profs, align_subsets)
+align_comp_4 <- base::Reduce(align_profs, align_subsets_4)
 
 tt4_2 <- Sys.time()
 
@@ -102,4 +102,4 @@ tt4_diff <- tt4_2 - tt4_1
 
 print(tt4_diff)
 
-print(align_comp)
+print(align_comp_4)
