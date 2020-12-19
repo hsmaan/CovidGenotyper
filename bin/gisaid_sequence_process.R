6116b3bfc39468f83cb60dbe1162fedd38a594f5
hsmaan <hmaan@uoguelph.ca>
2020-12-19 16:57:18 -0500

    testing locally with downsample to 1000

--- bin/gisaid_sequence_process.R
+++ bin/gisaid_sequence_process.R
@@ -48,22 +48,15 @@ ambg_freq_all <- which(((apply((letterFrequency(all_fastas, c("N", "W", "S", "M"
 
 all_fastas <- all_fastas[-ambg_freq_all]
 
-# Subset all files by available metadata and sample 10000
+# Subset all files by available metadata and sample 1000 sequences 
 
 all_fastas <- all_fastas[which(names(all_fastas) %in% meta_accession[,1])]
-
 meta_accession <- meta_accession[which(meta_accession[,1] %in% names(all_fastas)),]
-
 meta_accession <- data.frame("Accession" = as.character(meta_accession))
-
 accession_order <- base::match(names(all_fastas), meta_accession[,1])
-
 all_fastas <- all_fastas[order(accession_order)]
-
-all_10000 <- sample(names(all_fastas), 10000)
-
-all_fastas <- all_fastas[all_10000]
-
+all_1000 <- sample(names(all_fastas), 1000)
+all_fastas <- all_fastas[all_1000]
 pre_meta_sub <- pre_meta[which(pre_meta$Accession %in% names(all_fastas)),]
 
 # Remove gaps
