3aba020c088ec1dccd0ba0fc354d1336a02a6739
hsmaan <hmaan@uoguelph.ca>
2020-12-19 19:19:42 -0500

    fixed error in gisaid fasta length control + reupped sampling to 10k sequences

--- bin/gisaid_sequence_process.R
+++ bin/gisaid_sequence_process.R
@@ -48,23 +48,19 @@ ambg_freq_all <- which(((apply((letterFrequency(all_fastas, c("N", "W", "S", "M"
 
 all_fastas <- all_fastas[-ambg_freq_all]
 
-# Subset all files by available metadata and sample 1000 sequences 
+# Subset all files by available metadata and sample 10000 sequences 
 
 all_fastas <- all_fastas[which(names(all_fastas) %in% meta_accession[,1])]
 meta_accession <- meta_accession[which(meta_accession[,1] %in% names(all_fastas)),]
 meta_accession <- data.frame("Accession" = as.character(meta_accession))
 accession_order <- base::match(names(all_fastas), meta_accession[,1])
 all_fastas <- all_fastas[order(accession_order)]
-all_500 <- sample(names(all_fastas), 500)
-all_fastas <- all_fastas[all_500]
-pre_meta_sub <- pre_meta[which(pre_meta$Accession %in% names(all_fastas)),]
+all_10000 <- sample(names(all_fastas), 10000)
+all_fastas <- all_fastas[all_10000]
 
-# Remove gaps
+# Remove gaps and align
 
 all_fastas <- RemoveGaps(all_fastas, removeGaps = "all", processors = NULL)
-
-# Align 
-
 fasta_align <- AlignSeqs(all_fastas, iterations = 0, refinements = 0, processors = NULL)
 
 # Subset and exclude UTRs and gaps 
@@ -73,7 +69,6 @@ fasta_mat <- as.matrix(fasta_align)
 fasta_bin <- as.DNAbin(fasta_mat)
 fasta_ungapped <- del.colgapsonly(fasta_bin, threshold = 0.95)
 fasta_string <- fasta_ungapped %>% as.list %>% as.character %>% lapply(., paste0, collapse = "") %>% unlist %>% DNAStringSet
-
 fasta_final <- subseq(fasta_string, start = 265, end = 29674)
 
 writeXStringSet(fasta_string, file = paste("dec_aligned_fasta_filtered_", as.character(Sys.Date()), ".fasta", sep = ""))
@@ -92,10 +87,12 @@ fasta_masked <- subseq(align_masked, start = 265, end = 29674)
 
 fasta_dist <- dist.dna(as.DNAbin(fasta_masked), model = "K80", as.matrix = TRUE, pairwise.deletion = FALSE)
 
-# Append ref to alignment and output 
+# Append ref to alignment, subset for seqs not equal to 29903 in length, and output 
 
 fasta_align_ref <- c(ref_fasta, fasta_string)
+fasta_align_ref <- fasta_align_ref[which(length(fasta_align_ref) == 29903)]
 writeXStringSet(fasta_align_ref, file = paste("dec_aligned_plus_ref_filtered_", as.character(Sys.Date()), ".fasta", sep = ""))
+pre_meta_sub <- pre_meta[which(pre_meta$Accession %in% names(fasta_align_ref)),]
 
 # Output all objects 
 
